3a6a79601521fa5c8274c5fe0fd68eff53d05b28
Murilo Vasconcelos Andrade <muriloime@gmail.com>
2016-09-12 23:40:11 -0300

    add highlighting to zsh

--- fresh_start.sh
+++ fresh_start.sh
@@ -2,3 +2,6 @@
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
