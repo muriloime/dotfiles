fe64864f7591de4c5a0d0c72085d0403566191e3
Murilo Vasconcelos <muriloime@gmail.com>
2020-07-25 21:38:07 -0300

    remove useless

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,3 @@
 
 sudo apt-get install zsh-syntax-highlighting
-sudo apt-get install nautilus-sendto nautilus-actions nautilus-image-converter
-nautilus-compare nautilus-columns nautilus-admin nautilus-image-manipulator
-nautilus-hide nautilus-data
 
