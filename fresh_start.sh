61351e17637eb25a12cdb3df3fef463ef5c13f4a
Murilo Vasconcelos <muriloime@gmail.com>
2024-09-26 11:54:28 +0200

    fresh start

--- fresh_start.sh
+++ fresh_start.sh
@@ -58,6 +58,7 @@ chsh -s $(which zsh)
 ##### Oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# chsh -s $(which zsh)
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
