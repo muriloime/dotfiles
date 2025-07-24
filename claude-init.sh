b1183c2809cb19f6a0792fbfc1536281806e2b8b
Murilo Vasconcelos <muriloime@gmail.com>
2025-07-24 22:59:19 +0200

    claude init

---
+++ claude-init.sh
@@ -0,0 +1,3 @@
+claude mcp add --transport http context7 https://mcp.context7.com/mcp
+claude mcp add firecrawl -s user -- env FIRECRAWL_API_KEY=[INSERT KEY HERE] npx -y firecrawl-mcp
+claude mcp add playwright npx @playwright/mcp@latest
